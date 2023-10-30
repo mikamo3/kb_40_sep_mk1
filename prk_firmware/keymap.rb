e0317ec62f23f1a23d853df03f13d975d2f70d22
mikamo3 <kamo3proj@gmail.com>
2023-10-30 22:51:00 +0900

    wip

--- prk_firmware/keymap.rb
+++ prk_firmware/keymap.rb
@@ -17,10 +17,10 @@ KC_NO    KC_NO  KC_NO  KC_LALT KC_LGUI RAISE   ESC_CTL  BS_SFT  LOWER  KC_RGUI K
 
 )
 kbd.add_layer :raise, %i(
-KC_GRAVE KC_EXLM  KC_AT    KC_HASH  KC_DLR  KC_PERC  KC_DQUO  KC_QUOT  KC_CIRC  KC_AMPR  KC_ASTR  KC_LPRN  KC_RPRN  KC_DEL
+KC_GRAVE KC_EXLM  KC_AT    KC_HASH  KC_DLR  KC_PERC  KC_DQUO  KC_QUOT  KC_CIRC  KC_AMPR  KC_ASTER KC_LPRN  KC_RPRN  KC_DEL
 EN_JA    KC_1     KC_2     KC_3     KC_4    KC_5     KC_LCBR  KC_RCBR  KC_6     KC_7     KC_8     KC_9     KC_0     KC_GRV
 KC_NO    KC_Z     KC_X     KC_C     KC_V    KC_B     KC_NO    KC_NO    KC_N     KC_M     KC_COMM  KC_DOT   KC_SLSH  KC_NO
-KC_NO    KC_NO    KC_NO    KC_LALT  KC_LGUI RAISE    KC_LCTL  KC_RSFT  LOWER    KC_RGUI  KC_RALT  KC_NO    KC_NO  KC_NO
+KC_NO    KC_NO    KC_NO    KC_LALT  KC_LGUI RAISE    KC_LCTL  KC_DEL   LOWER    KC_RGUI  KC_RALT  KC_NO    KC_NO  KC_NO
 
 )
 kbd.add_layer :lower, %i(
@@ -30,12 +30,26 @@ KC_NO   KC_PSCREEN KC_INS  KC_SLCK KC_PAUS KC_B   KC_NO   KC_NO   KC_N    KC_M  
 KC_NO   KC_NO   KC_NO   KC_LALT KC_LGUI RAISE  KC_LCTL KC_RSFT LOWER   KC_RGUI KC_RALT KC_NO   KC_NO   KC_NO
 
 )
+
+kbd.add_layer :ctrl, [
+:ESC_CTL, :KC_Q,  :KC_W,  :KC_E,   :KC_R,   :KC_T,   :KC_MINS, :KC_EQL, :KC_Y,  :KC_U,   :KC_I,   :KC_O,   :KC_P,   :KC_BSPC,
+:TAB_SFT, :KC_A,  :KC_S,  :KC_D,   :KC_F,   :KC_G,   :KC_LBRC, :KC_RBRC,:KC_H,  :KC_J,   :KC_K,   :KC_L,   :KC_SCLN,:KC_BSLS,
+:KC_NO,   :KC_Z,  :KC_X,  :KC_C,   :KC_V,   :KC_B,   :KC_NO,   :KC_NO,  :KC_N,  :KC_M,   :KC_COMM,:KC_DOT, :KC_SLSH,:KC_NO,
+:KC_NO,   :KC_NO, :KC_NO, :KC_LALT,:KC_LGUI,:RAISE,  :ESC_CTL, :BS_SFT, :LOWER, :KC_RGUI,:KC_RALT,:KC_NO,  :KC_NO,  :KC_NO
+]
+kbd.add_layer :shift, [
+:ESC_CTL, :KC_Q,  :KC_W,  :KC_E,   :KC_R,   :KC_T,   :KC_MINS, :KC_EQL, :KC_Y,  :KC_U,   :KC_I,   :KC_O,   :KC_P,   :KC_BSPC,
+:TAB_SFT, :KC_A,  :KC_S,  :KC_D,   :KC_F,   :KC_G,   :KC_LBRC, :KC_RBRC,:KC_H,  :KC_J,   :KC_K,   :KC_L,   :KC_SCLN,:KC_BSLS,
+:KC_NO,   :KC_Z,  :KC_X,  :KC_C,   :KC_V,   :KC_B,   :KC_NO,   :KC_NO,  :KC_N,  :KC_M,   :KC_COMM,:KC_DOT, :KC_SLSH,:KC_NO,
+:KC_NO,   :KC_NO, :KC_NO, :KC_LALT,:KC_LGUI,:RAISE,  :ESC_CTL, :BS_SFT, :LOWER, :KC_RGUI,:KC_RALT,:KC_NO,  :KC_NO,  :KC_NO
+]
+
 kbd.define_mode_key :RAISE, [ :KC_SPACE, :raise, 200, 200 ]
 kbd.define_mode_key :LOWER, [ :KC_ENT, :lower, 200, 200 ]
 kbd.define_mode_key :TAB_SFT, [ :KC_TAB, :KC_LSFT, 200, 200 ]
 kbd.define_mode_key :ESC_CTL, [ :KC_ESC, :KC_LCTL, 200, 200 ]
 kbd.define_mode_key :BS_SFT, [ :KC_BSPC, :KC_RSFT, 200, 200 ]
-kbd.define_composite_key :EN_JA,   %i(KC_LALT KC_GRAVE)
+kbd.define_composite_key :EN_JA,   %i(KC_GRAVE KC_LALT )
 
 encoder_1 = RotaryEncoder.new(14, 15)
 encoder_1.configure :left
